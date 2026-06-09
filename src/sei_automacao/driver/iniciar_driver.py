991807c7ad2a88491c95a60b3bbcbec61481c804
Gustavo Amaral Bernardino <gu.amaralb@gmail.com>
2026-06-09 16:23:15 -0300

    Refactor iniciar_driver function to include proxy settings and options for Chrome WebDriver

--- src/sei_automacao/driver/iniciar_driver.py
+++ src/sei_automacao/driver/iniciar_driver.py
@@ -1,10 +1,16 @@
+import os
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def iniciar_driver():
-    service = Service(ChromeDriverManager().install())    
-    driver = webdriver.Chrome(service=service)
-
+    os.environ['NO_PROXY'] = 'localhost,127.0.0.1'
+    os.environ['no_proxy'] = 'localhost,127.0.0.1'
+    options = Options()
+    options.add_argument('--no-proxy-server')
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
