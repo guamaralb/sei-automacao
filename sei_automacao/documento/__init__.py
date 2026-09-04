6598805f9816edd2508dca4b04b59464cdb8c841
Gustavo Amaral Bernardino <gu.amaralb@gmail.com>
2026-09-04 16:08:09 -0300

    feat: Add function to retrieve document content in different formats in __init__.py

--- sei_automacao/documento/__init__.py
+++ sei_automacao/documento/__init__.py
@@ -49,3 +49,38 @@ def inserir_bloco_ass(
     clicar_incluir_sbmIncluir(driver)
     time.sleep(2)
     driver.switch_to.default_content()
+
+import time
+import os
+
+from pathlib import Path
+from typing import Literal
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import StaleElementReferenceException
+
+from sei_automacao.utils.acesso import selecionar_nivel_acesso
+from sei_automacao.core.iframes import trocar_iframe
+
+
+def obter_conteudo_doc(driver: webdriver.Remote, formato: Literal['web_element', 'texto'] = 'texto') -> str | WebElement:
+    driver.switch_to.default_content()
+    trocar_iframe(driver, 'ifrConteudoVisualizacao')
+    trocar_iframe(driver, 'ifrVisualizacao')
+
+    body = driver.find_element(By.TAG_NAME, 'body')
+
+    texto = body.text
+
+    if formato == 'html':
+        return body
+    elif formato == 'texto':
+        return texto
+
+
+
+
